f859fe0059b7aa84df8328b66f16ba0d2712faf3
root <biyayibash@gmail.com>
2025-03-07 21:03:45 +0100

    Mon premier commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Le Rocher</title>
+</head>
+<body>
+    <h1><?php echo "Groupe Rocher Musique"; ?></h1>
+</body>
+</html>
